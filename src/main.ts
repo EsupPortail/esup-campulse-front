0f9672ab029952cb3f8467fe49e9d39949537db4
marco <marco@unistra.fr>
2022-10-25 15:20:09 +0200

    ➕ Add vueI18n

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { createPinia } from 'pinia'
 
 import App from '@/App.vue'
 import router from '@/router'
+import i18n from '@/plugins/i18n'
 
 const app = createApp(App)
 
+app.use(i18n)
 app.use(createPinia())
 app.use(router)
 
