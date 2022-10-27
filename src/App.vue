d8a9b62fc3e2bcaef4138f754a1c8b8d5b30b6df
Carolane Lefebvre <carolanelefebvre@>
2022-10-27 12:44:34 +0200

    :lipstick: Updated styles files

--- src/App.vue
+++ src/App.vue
@@ -27,11 +27,12 @@ import { RouterLink, RouterView } from 'vue-router'
 
       <q-footer elevated>
         <q-toolbar>
-          <q-toolbar-title class="test">2017 - 2022 - PLATEFORME - Tous droits réservés</q-toolbar-title>
-          <RouterLink to="/" class="li">A propos</RouterLink>
-          <RouterLink to="/" class="li">Mentions légales</RouterLink>
-          <RouterLink to="/" class="li">Contact</RouterLink>
-
+          <div class="footer-text">
+            <q-toolbar-title>2017-2022 - PLATEFORME - Tous droits réservés</q-toolbar-title>
+            <RouterLink to="/" class="li-footer">A propos</RouterLink>
+            <RouterLink to="/" class="li-footer">Mentions légales</RouterLink>
+            <RouterLink to="/" class="li-footer">Contact</RouterLink>
+          </div>
         </q-toolbar>
       </q-footer>
 
@@ -41,25 +42,35 @@ import { RouterLink, RouterView } from 'vue-router'
 <style lang="sass">
 @import "@/assets/base.css"
 
-nav
-  display: flex
-  justify-content: flex-end
-
-.li
-  color: white
-  padding: 1em
-
-.li:hover
-  background-color: rgba(255, 255, 255, 0.3)
-
+/** Layout **/
 .q-layout
   max-width: 1280px
   margin: 0 auto
 
-q-footer
+/** Nav **/
+nav
+  display: flex
+  justify-content: flex-end
 
-  .test
-    display: flex
-    justify-content: center
+.li, .li-footer
+  color: white
+  padding: 0.3em
+
+.li:hover
+  background-color: rgba(255, 255, 255, 0.3)
+
+/** Footer **/
+.q-toolbar
+  justify-content: center
+
+.footer-text
+  text-align: center
+
+.li-footer:after
+  content: "|"
+  padding-left: 0.5em
+
+.li-footer:last-child:after
+  content: none
 
 </style>
