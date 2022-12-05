aa57a5019ac10f8cd5a5ccd155309bc2f9cd2128
Carolane Lefebvre <carolanelefebvre@>
2022-12-05 13:12:07 +0100

    :construction: Association Details View

--- src/views/AssociationDetailView.vue
+++ src/views/AssociationDetailView.vue
@@ -11,6 +11,7 @@ const {notify} = useQuasar()
 
 const associationStore = useAssociationStore()
 
+// TODO: Add socials on Association interface & president_mail
 
 // onMounted(getAssociation)
 onBeforeMount(getAssociation)
@@ -36,7 +37,7 @@ async function getAssociation() {
             <img v-if="associationStore.association.pathLogo" :src="associationStore.association.pathLogo" alt=""/>
             <div v-else></div>
         </div>
-        <div class="name">
+        <div class="name/">
             <h1>{{ associationStore.association.name }}</h1>
             <span>Charte valide jusqu'au XX/XX/XX</span>
         </div>
@@ -72,11 +73,57 @@ async function getAssociation() {
             <p>{{ associationStore.association.institution.name }}</p>
         </div>
 
+      <div class="cardbox-item">
+        <p>{{ associationStore.association.phone }}</p>
+      </div>
+
+      <div class="cardbox-item">
+        <p>{{ associationStore.association.email }}</p>
+      </div>
+
+      <div class="cardbox-item">
+        <p>{{ associationStore.association.siret }}</p>
+      </div>
+
+      <div class="cardbox-item">
+        <p>{{ associationStore.association.lastGoaDate }}</p>
+      </div>
+
+      <div class="cardbox-item">
+        <p>{{ associationStore.association.cgaDate }}</p>
+      </div>
     </div>
 
     <div class="cardbox-title">
         <h2>Contact</h2>
+
+      <div class="cardbox-item">
+        <p>{{ associationStore.association.address }}</p>
+      </div>
+
+      <div class="cardbox-item">
+        <p>{{ associationStore.association.email }}</p>
+      </div>
+
+      <div class="cardbox-item">
+        <p>{{ associationStore.association.website }}</p>
+      </div>
+
+      <div class="cardbox-item">
+        <p>{{ associationStore.association.website }}</p>
+      </div>
     </div>
+
+    <QBtn
+        :href="CASUrlLogin"
+        :label="$t('association.contact-association')"
+        color="primary"
+    />
+    <QBtn
+        :href="CASUrlLogin"
+        :label="$t('association.back-directory')"
+        color="primary"
+    />
 </template>
 
 <style lang="sass" scoped>
