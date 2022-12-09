e0a33d0e1cc1de8ac0efe7b2d6d906f3e0b19f70
Carolane Lefebvre <carolane.lefebvre@unistra.fr>
2022-12-09 11:19:27 +0100

    :recycle: FIX: Refactor User Detail View

--- src/views/UsersView.vue
+++ src/views/UsersView.vue
@@ -7,77 +7,74 @@ import router from '@/router'
 import {useI18n} from 'vue-i18n'
 
 const {t} = useI18n()
-const {notify} = useQuasar()
+const {notify, loading} = useQuasar()
 const userManagerStore = useUserManagerStore()
-const {loading} = useQuasar()
-
-// onMounted(userManagerStore.getUsers)
 
 onMounted(async () => {
-    loading.show
-    await userManagerStore.getUsersAdmitted(true)
-    loading.hide
+  loading.show
+  await userManagerStore.getUsersAdmitted(true)
+  loading.hide
 })
 
 const columns: QTableProps['columns'] = [
-    {
-        name: 'id',
-        required: true,
-        label: 'ID',
-        align: 'left',
-        field: row => row.id,
-        format: val => `${val}`,
-        sortable: true
-    },
-    {name: 'firstName', align: 'left', label: 'Nom de famille', field: 'firstName', sortable: true},
-    {name: 'lastName', align: 'left', label: 'Prénom', field: 'lastName', sortable: true},
-    {name: 'email', align: 'left', label: 'Adresse mail', field: 'email', sortable: true},
-    {
-        name: 'isValidatedByAdmin',
-        align: 'left',
-        label: 'Validé par le gestionnaire',
-        field: 'isValidatedByAdmin',
-        sortable: true
-    },
+  {
+    name: 'id',
+    required: true,
+    label: 'ID',
+    align: 'left',
+    field: row => row.id,
+    format: val => `${val}`,
+    sortable: true
+  },
+  {name: 'firstName', align: 'left', label: 'Nom de famille', field: 'firstName', sortable: true},
+  {name: 'lastName', align: 'left', label: 'Prénom', field: 'lastName', sortable: true},
+  {name: 'email', align: 'left', label: 'Adresse mail', field: 'email', sortable: true},
+  {
+    name: 'isValidatedByAdmin',
+    align: 'left',
+    label: 'Validé par le gestionnaire',
+    field: 'isValidatedByAdmin',
+    sortable: true
+  },
 ]
 
 function goTo(id: number) {
-    router.push({name: 'UserDetail', params: {id}})
+  router.push({name: 'UserDetail', params: {id}})
 }
 </script>
 
 <template>
-    <h1>{{ t("manager.users") }}</h1>
-    <QTable
-        :columns="columns"
-        :rows="userManagerStore.userDirectory"
-        row-key="id"
-        title="Users"
-    >
-        <template v-slot:body="props">
-            <QTr :props="props" @click="goTo(props.row.id)">
-                <QTd key="id" :props="props">
-                    {{ props.row.id }}
-                </QTd>
-                <QTd key="firstName" :props="props">
-                    {{ props.row.firstName }}
-                </QTd>
-                <QTd key="lastName" :props="props">
-                    {{ props.row.lastName }}
-                </QTd>
-                <QTd key="email" :props="props">
-                    {{ props.row.email }}
-                </QTd>
-                <QTd key="isValidatedByAdmin" :props="props">
-                    {{ props.row.isValidatedByAdmin ? "Oui" : "Non" }}
-                </QTd>
-            </QTr>
-        </template>
-    </QTable>
+  <h1>{{ t("manager.users") }}</h1>
+  <QTable
+      :columns="columns"
+      :rows="userManagerStore.userDirectory"
+      row-key="id"
+      title="Users"
+  >
+    <template v-slot:body="props">
+      <QTr :props="props" @click="goTo(props.row.id)">
+        <QTd key="id" :props="props">
+          {{ props.row.id }}
+        </QTd>
+        <QTd key="firstName" :props="props">
+          {{ props.row.firstName }}
+        </QTd>
+        <QTd key="lastName" :props="props">
+          {{ props.row.lastName }}
+        </QTd>
+        <QTd key="email" :props="props">
+          {{ props.row.email }}
+        </QTd>
+        <QTd key="isValidatedByAdmin" :props="props">
+          {{ props.row.isValidatedByAdmin ? "Oui" : "Non" }}
+        </QTd>
+      </QTr>
+    </template>
+  </QTable>
 </template>
 
 <style lang="sass" scoped>
 .q-tr:hover
-    cursor: pointer
+  cursor: pointer
 </style>
 
