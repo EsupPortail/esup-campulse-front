d1ec1550e3bb44f71b708a40ce82460f5a241da6
pgilg <pgilg@unistra.fr>
2022-12-15 10:11:11 +0100

    🎨 Minor typo changes

--- src/components/form/FormNewAssociation.vue
+++ src/components/form/FormNewAssociation.vue
@@ -12,74 +12,74 @@ const {createAssociation} = useAssociation();
 const newAssociation = ref<string>('')
 
 async function onCreate() {
-  try {
-    await createAssociation(newAssociation.value)
-    await router.push({name: `Dashboard`})
-    notify({
-      type: 'positive',
-      message: t('notifications.positive.validate-asso')
-    })
-  } catch (e) {
-    notify({
-      type: 'negative',
-      message: t('notifications.negative.error-asso')
-    })
-  }
+    try {
+        await createAssociation(newAssociation.value)
+        await router.push({name: 'Dashboard'})
+        notify({
+            type: 'positive',
+            message: t('notifications.positive.validate-asso')
+        })
+    } catch (e) {
+        notify({
+            type: 'negative',
+            message: t('notifications.negative.error-asso')
+        })
+    }
 }
 </script>
 
 <template>
-  <QForm
-      class="q-gutter-md"
-      @submit.prevent="onCreate"
-  >
-    <h3>{{ t("association.labels.new-asso") }}</h3>
-    <QInput
-        v-model="newAssociation"
-        :label="t('forms.name')"
-        filled
-        lazy-rules
-    />
-    <section class="btn-group">
-      <QBtn
-          :label="t('home.back-dashboard')"
-          color="secondary"
-          icon="mdi-arrow-left-circle"
-          to="/associations"
-      />
-      <QBtn
-          :label="t('manager.validate')"
-          color="primary"
-          @:click="onCreate"
-      />
-    </section>
-  </QForm>
+    <QForm
+        class="q-gutter-md"
+        @submit.prevent="onCreate"
+    >
+        <h3>{{ t("association.labels.new-asso") }}</h3>
+        <QInput
+            v-model="newAssociation"
+            :label="t('forms.name')"
+            filled
+            lazy-rules
+        />
+        <section class="btn-group">
+            <QBtn
+                :label="t('home.back-dashboard')"
+                color="secondary"
+                icon="mdi-arrow-left-circle"
+                to="/associations"
+            />
+            <QBtn
+                :label="t('manager.validate')"
+                color="primary"
+                @:click="onCreate"
+            />
+        </section>
+    </QForm>
 </template>
 
 <style lang="sass" scoped>
 h2
-  background-color: $primary
-  color: #fff
-  font-size: 2em
-  text-align: center
+    background-color: $primary
+    color: #fff
+    font-size: 2em
+    text-align: center
 
 article > *
-  margin: 0
-  width: 50%
+    margin: 0
+    width: 50%
 
 article
-  display: flex
-  align-items: center
-  background-color: lightgrey
-  padding: 0 20px 0 20px
-  margin: 5px 0
+    display: flex
+    align-items: center
+    background-color: lightgrey
+    padding: 0 20px 0 20px
+    margin: 5px 0
 
 h3
-  font-size: 1.2em
-  text-transform: uppercase
+    font-size: 1.2em
+    text-transform: uppercase
 
 .btn-group
-  display: flex
-  gap: 10px
+    display: flex
+    gap: 10px
 
 </style>
--- src/composables/useAssociation.ts
+++ src/composables/useAssociation.ts
@@ -2,7 +2,7 @@ import _axios from "@/plugins/axios";
 
 export default function () {
     async function createAssociation(name: string) {
-        await _axios.post(`/associations/`, {name: name})
+        await _axios.post('/associations/', {name: name})
     }
 
     return {createAssociation}
