9f779b7670b1e9195d2f53b00ef9f0b7eae5185b
pgilg <pgilg@unistra.fr>
2023-01-23 16:06:52 +0100

    💄 Prepare for styling

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import { Loading, Notify, Quasar } from 'quasar'
+import {createApp} from 'vue'
+import {createPinia} from 'pinia'
+import {Loading, Notify, Quasar} from 'quasar'
 import quasarLang from 'quasar/lang/fr'
 import quasarIconSet from 'quasar/icon-set/svg-mdi-v6'
 import '@quasar/extras/mdi-v6/mdi-v6.css'
 import 'quasar/dist/quasar.css'
 import * as Sentry from '@sentry/vue'
-import { BrowserTracing } from '@sentry/tracing'
+import {BrowserTracing} from '@sentry/tracing'
 import App from '@/App.vue'
 import router from '@/router'
 import i18n from '@/plugins/i18n'
@@ -42,10 +42,6 @@ app.use(Quasar, {
     },
     config: {
         notify: {},
-        brand: {
-            primary: "#025827",
-            secondary: "#1976D2"
-        }
     },
     lang: quasarLang,
     iconSet: quasarIconSet
