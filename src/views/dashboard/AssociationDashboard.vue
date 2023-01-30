f08384d96304325678911c8033d6483be10b6515
Carolane Lefebvre <carolane.lefebvre@unistra.fr>
2023-01-30 15:05:43 +0100

    :construction: [22] WIP: AssociationDashboard View

---
+++ src/views/dashboard/AssociationDashboard.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import {useUserStore} from '@/stores/useUserStore'
+import {useI18n} from 'vue-i18n'
+
+const {t} = useI18n()
+const {getUser} = useUsers()
+
+async function onGetUser() {
+  try {
+    await getUser()
+  }
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="sass" scoped>
+</style>
