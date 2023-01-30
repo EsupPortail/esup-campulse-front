a83bf22ea6149e280fa0f20744bc46e1341f8647
Antoine Latour <antoinelatour@hotmail.fr>
2023-01-30 15:08:00 +0100

    Footer styling

--- src/components/layout/LayoutFooter.vue
+++ src/components/layout/LayoutFooter.vue
@@ -6,14 +6,30 @@ const {t} = useI18n()
 </script>
 
 <template>
-    <QFooter elevated>
+    <QFooter id="layout-footer" class="variant-space-1" elevated>
+        <div id="footer-logos">
+            <div class="wrapper">
+                <img src="@/assets/img/logos/uha.png" />
+                <img src="@/assets/img/logos/hear-white.png" />
+                <img src="@/assets/img/logos/insa.png" />
+                <img src="@/assets/img/logos/ensas.png" />
+                <img src="@/assets/img/logos/engees.png" />
+                <img src="@/assets/img/logos/unistra.png" />
+            </div>
+        </div>
         <QToolbar>
             <div class="footer-text">
-                <QToolbarTitle>{{ t("footer.copyright") }}</QToolbarTitle>
-                <RouterLink class="li-footer" to="/">{{ t("footer.about") }}</RouterLink>
-                <RouterLink class="li-footer" to="/">{{ t("footer.legal-notice") }}</RouterLink>
-                <RouterLink class="li-footer" to="/">{{ t("footer.contact") }}</RouterLink>
+                <RouterLink class="li-footer" to="/"><span class="icon icon-mentions"></span>{{ t("footer.about") }}</RouterLink>
+                <RouterLink class="li-footer" to="/"><span class="icon icon-contact-crous"></span>{{ t("footer.legal-notice") }}</RouterLink>
+                <RouterLink class="li-footer" to="/"><span class="icon icon-contact-admin"></span>{{ t("footer.contact") }}</RouterLink>
+                <!-- <QToolbarTitle>{{ t("footer.copyright") }}</QToolbarTitle> -->
             </div>
         </QToolbar>
+        <p>
+            La Base de Données des Associations Etudiantes d’Alsace est un annuaire regroupant les associations étudiantes d’Alsace. 
+            Ce projet est réalisé dans le cadre du Schéma Directeur de la Vie Etudiante – Alsace, qui lie les établissements d’enseignement supérieur 
+            (Unistra, UHA, INSA, ENGEES, ENSAS, HEAR) et la BNU, en partenariat avec le Crous.
+        </p>
+        <p>{{ t("footer.copyright") }}</p>
     </QFooter>
 </template>
