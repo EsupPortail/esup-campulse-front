2f33c95a8e065368e50cf0147913c1e80c0a36d4
Antoine Latour <antoinelatour@hotmail.fr>
2023-02-07 17:48:07 +0100

    Fixed the style of the header menu links when focused + added the underline effect on hover

--- src/components/layout/LayoutHeaderNav.vue
+++ src/components/layout/LayoutHeaderNav.vue
@@ -11,9 +11,9 @@ const userStore = useUserStore()
 <template>
     <nav>
         <RouterLink to="/">{{ t("header.home") }}</RouterLink>
-        <RouterLink to="/charter">{{ t("header.charter") }}</RouterLink>
-        <RouterLink to="/associations">{{ t("header.associations") }}</RouterLink>
-        <RouterLink to="/commission">{{ t("header.commission") }}</RouterLink>
+        <RouterLink class="space-2" to="/charter">{{ t("header.charter") }}</RouterLink>
+        <RouterLink class="space-1" to="/associations">{{ t("header.associations") }}</RouterLink>
+        <RouterLink class="space-3" to="/commission">{{ t("header.commission") }}</RouterLink>
         <RouterLink v-if="!userStore.isAuth" to="/login">{{ t("header.login") }}</RouterLink>
         <LayoutHeaderProfileButton class="profile-button" v-if="userStore.isAuth"/>
     </nav>
