796b045e16420a47ab55b6909b827516e83f52da
Carolane Lefebvre <carolane.lefebvre@unistra.fr>
2023-02-14 10:33:47 +0100

    :recycle: [13] - CHANGE: Checkbox's label

--- src/components/form/FormNewAssociation.vue
+++ src/components/form/FormNewAssociation.vue
@@ -18,107 +18,107 @@ const {hasPerm} = useSecurity()
 
 
 const newAssociation = reactive<NewAssociation>({
-    name: '',
-    institution: undefined,
-    isSite: false
+  name: '',
+  institution: undefined,
+  isSite: false
 })
 
 const institutions = ref<{ value: number, label: string }[]>([])
 
 const initValues = () => {
-    institutions.value = associationStore.institutions.map(function (institution) {
-        return {
-            value: institution.id,
-            label: institution.name
-        }
-    })
-    if (!hasPerm('add_association_any_institution')) {
-        newAssociation.institution = userStore.user?.groups[0].institutionId
+  institutions.value = associationStore.institutions.map(function (institution) {
+    return {
+      value: institution.id,
+      label: institution.name
     }
+  })
+  if (!hasPerm('add_association_any_institution')) {
+    newAssociation.institution = userStore.user?.groups[0].institutionId
+  }
 }
 watch(() => associationStore.institutions, initValues)
 
 onMounted(async () => {
-    await associationStore.getInstitutions()
+  await associationStore.getInstitutions()
 })
 
 async function onCreate() {
-    try {
-        await createAssociation(newAssociation)
-        notify({
-            type: 'positive',
-            message: t('notifications.positive.validate-association')
-        })
-    } catch (error) {
-        if (axios.isAxiosError(error) && error.response?.data.error === 'Association name already taken.') {
-            notify({
-                type: 'negative',
-                message: t('notifications.negative.association-already-exists')
-            })
-        } else {
-            notify({
-                type: 'negative',
-                message: t('notifications.negative.error-new-association')
-            })
-        }
+  try {
+    await createAssociation(newAssociation)
+    notify({
+      type: 'positive',
+      message: t('notifications.positive.validate-association')
+    })
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response?.data.error === 'Association name already taken.') {
+      notify({
+        type: 'negative',
+        message: t('notifications.negative.association-already-exists')
+      })
+    } else {
+      notify({
+        type: 'negative',
+        message: t('notifications.negative.error-new-association')
+      })
     }
+  }
 }
 
 const clearValues = () => {
-    newAssociation.name = ''
-    newAssociation.institution = undefined
-    newAssociation.isSite = false
+  newAssociation.name = ''
+  newAssociation.institution = undefined
+  newAssociation.isSite = false
 }
 
 </script>
 
 <template>
-    <QForm
-        class="q-gutter-md"
-        @submit.prevent="onCreate"
-        @reset-validation="clearValues"
-    >
-        <QInput
-            v-model="newAssociation.name"
-            :label="t('forms.association-name')"
-            :rules="[ val => val.length > 0 || t('forms.fill-field') ]"
-            filled
-            lazy-rules
-        />
-        <QSelect
-            v-model="newAssociation.institution"
-            :label="t('forms.association-institution')"
-            :options="institutions"
-            :readonly="!hasPerm('add_association_any_institution')"
-            :rules="[ val => val !== undefined || t('forms.select-option') ]"
-            emit-value
-            filled
-            lazy-rules
-            map-options
-        />
-        <QCheckbox
-            v-model="newAssociation.isSite"
-            :label="t('forms.association-is-site')"
-        />
-        <section class="btn-group">
-            <QBtn
-                :label="t('home.back-dashboard')"
-                :to="{name: 'Dashboard'}"
-                color="secondary"
-                icon="mdi-arrow-left-circle"
-            />
-            <QBtn
-                :label="t('user-manager.create-association')"
-                color="primary"
-                icon="mdi-check-circle"
-                type="submit"
-            />
-        </section>
-    </QForm>
+  <QForm
+      class="q-gutter-md"
+      @submit.prevent="onCreate"
+      @reset-validation="clearValues"
+  >
+    <QInput
+        v-model="newAssociation.name"
+        :label="t('forms.association-name')"
+        :rules="[ val => val.length > 0 || t('forms.fill-field') ]"
+        filled
+        lazy-rules
+    />
+    <QSelect
+        v-model="newAssociation.institution"
+        :label="t('forms.association-institution')"
+        :options="institutions"
+        :readonly="!hasPerm('add_association_any_institution')"
+        :rules="[ val => val !== undefined || t('forms.select-option') ]"
+        emit-value
+        filled
+        lazy-rules
+        map-options
+    />
+    <QCheckbox
+        v-model="newAssociation.isSite"
+        :label="t('forms.association-is-site')"
+    />
+    <section class="btn-group">
+      <QBtn
+          :label="t('home.back-dashboard')"
+          :to="{name: 'Dashboard'}"
+          color="secondary"
+          icon="mdi-arrow-left-circle"
+      />
+      <QBtn
+          :label="t('user-manager.create-association')"
+          color="primary"
+          icon="mdi-check-circle"
+          type="submit"
+      />
+    </section>
+  </QForm>
 </template>
 
 <style lang="sass" scoped>
 .btn-group
-    display: flex
-    gap: 10px
+  display: flex
+  gap: 10px
 </style>
