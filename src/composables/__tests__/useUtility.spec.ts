6996d68be481ca1e4f4f8aa61dffd2caca55f8b4
pgilg <pgilg@unistra.fr>
2023-03-02 10:41:26 +0100

    ✅ Add a test for IDs in path when initBreadcrumbs

--- src/composables/__tests__/useUtility.spec.ts
+++ src/composables/__tests__/useUtility.spec.ts
@@ -6,37 +6,46 @@ import useUtility from '@/composables/useUtility'
 vi.mock('vue-router', () => ({useRoute: vi.fn()}))
 
 describe('useUtility', () => {
+
     describe('formatDate', () => {
+        const {formatDate} = useUtility()
+
         it('should return formatted date', () => {
-            const {formatDate} = useUtility()
             expect(formatDate('2022-10-27 13:45:35.000000 +00:00')).toEqual('2022-10-27')
         })
+
         it('should not return a date with an error', () => {
-            const {formatDate} = useUtility()
             expect(formatDate('2022-10-27 13:45:35.000000 +00:00')).not.toEqual('2022-10-28')
         })
+
         it('should return nothing if no date in arg', () => {
-            const {formatDate} = useUtility()
             expect(formatDate('')).toBeUndefined()
         })
     })
+
     describe('arraysAreEqual', () => {
         const {arraysAreEqual} = useUtility()
         const a = [1, 2, 3]
         const b = [3, 1, 2]
         const c = [1, 2, 4]
         const d = [4, 5, 6, 7]
+
         it('should be false if arrays have not the same length', () => {
             expect(arraysAreEqual(a, d)).toBeFalsy()
         })
+
         it('should be false if arrays do not contain the same values', () => {
             expect(arraysAreEqual(a, c)).toBeFalsy()
         })
+
         it('should be true if arrays contain the same values', () => {
             expect(arraysAreEqual(a, b)).toBeTruthy()
         })
     })
+
     describe('initBreadcrumbs', () => {
+        const {initBreadcrumbs} = useUtility()
+
         it('should return an array of breadcrumb with label and to based on the route', () => {
             vi.mocked(useRoute).mockReturnValue({
                 name: 'ValidateUsers', matched: [
@@ -55,7 +64,6 @@ describe('useUtility', () => {
                 ]
             } as RouteLocationNormalizedLoaded)
             const route = useRoute()
-            const {initBreadcrumbs} = useUtility()
             initBreadcrumbs(route.matched, route.params)
             expect(initBreadcrumbs(route.matched, route.params)).toEqual([
                 {
@@ -65,6 +73,40 @@ describe('useUtility', () => {
                 {
                     label: 'Valider des comptes',
                     to: '/dashboard/validate-users'
+                }
+            ])
+        })
+
+        it('should get an id from params if there is an id in the route path', () => {
+            // @ts-ignore For testing purpose only
+            vi.mocked(useRoute).mockReturnValue({
+                name: 'AssociationDetail',
+                params: {id: '1'},
+                matched: [
+                    {
+                        path: '/',
+                        meta: {}
+                    },
+                    {
+                        path: '/associations',
+                        meta: {breadcrumb: 'Annuaire des associations'}
+                    },
+                    {
+                        path: '/associations/:id',
+                        meta: {breadcrumb: 'Fiche association'},
+                    }
+                ]
+            } as RouteLocationNormalizedLoaded)
+            const route = useRoute()
+            initBreadcrumbs(route.matched, route.params)
+            expect(initBreadcrumbs(route.matched, route.params)).toEqual([
+                {
+                    label: 'Annuaire des associations',
+                    to: '/associations'
+                },
+                {
+                    label: 'Fiche association',
+                    to: '/associations/1'
                 }
             ])
         })
