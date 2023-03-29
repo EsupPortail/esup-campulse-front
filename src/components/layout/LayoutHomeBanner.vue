501e72c786fef399e18a78fc258dafeecb79ef2a
Carolane Lefebvre <carolane.lefebvre@unistra.fr>
2023-03-29 10:29:17 +0200

    :lipstick: feat: banner (homeview) is now responsive

--- src/components/layout/LayoutHomeBanner.vue
+++ src/components/layout/LayoutHomeBanner.vue
@@ -1,27 +1,37 @@
 <script lang="ts" setup>
 defineProps({
-    title: String,
-    description: String,
-    isDisplayed: Boolean
+  title: String,
+  description: String,
+  isDisplayed: Boolean
 })
 </script>
 
 <template>
-    <QBanner v-if="isDisplayed" class="bg-primary text-white">
-        <div id="home-banner-icon" class="variant-default">
-            <i class="bi bi-info-circle"></i>
-        </div>
+  <QBanner v-if="isDisplayed" class="bg-primary text-white">
+    <div id="home-banner-icon" class="variant-default">
+      <i class="bi bi-info-circle"></i>
+    </div>
 
-        <QCardSection>
-            <div class="text-h6">{{ title }}</div>
-        </QCardSection>
+    <QCardSection>
+      <div class="text-h6">{{ title }}</div>
+    </QCardSection>
 
-        <QCardSection class="q-pt-none">
-            {{ description }}
-        </QCardSection>
+    <QCardSection class="q-pt-none">
+      {{ description }}
+    </QCardSection>
 
-        <QCardSection class="q-pt-none">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        </QCardSection>
-    </QBanner>
+    <QCardSection class="q-pt-none">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+      magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat.
+    </QCardSection>
+  </QBanner>
 </template>
+
+<style lang="scss" scoped>
+@import "@/assets/_variables.scss";
+
+.text-h6 {
+  font-weight: $xbold-weight;
+}
+</style>
