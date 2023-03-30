3626624fc055dbe887e869722362a45b3b9eb028
Carolane Lefebvre <carolane.lefebvre@unistra.fr>
2023-03-30 11:25:15 +0200

    :lipstick: fix: dashboard password edit view

--- src/components/form/FormUserInfosEdition.vue
+++ src/components/form/FormUserInfosEdition.vue
@@ -11,8 +11,8 @@ const {getGroupLiteral} = useUserGroups()
 const {t} = useI18n()
 
 const props = defineProps<{
-    editedByStaff: boolean,
-    user: User
+  editedByStaff: boolean,
+  user: User
 }>()
 
 const userRef = toRefs(props).user
@@ -22,112 +22,112 @@ const changeEmail = ref<boolean>(false)
 const userGroups = ref<string>('')
 
 const initUserInfos = () => {
-    userToUpdate.value.firstName = userRef.value.firstName
-    userToUpdate.value.lastName = userRef.value.lastName
-    userToUpdate.value.username = userRef.value.username
-    userToUpdate.value.email = userRef.value.email
-    userToUpdate.value.newEmail = ''
-    userToUpdate.value.newEmailVerification = ''
-    userToUpdate.value.phone = userRef.value.phone
+  userToUpdate.value.firstName = userRef.value.firstName
+  userToUpdate.value.lastName = userRef.value.lastName
+  userToUpdate.value.username = userRef.value.username
+  userToUpdate.value.email = userRef.value.email
+  userToUpdate.value.newEmail = ''
+  userToUpdate.value.newEmailVerification = ''
+  userToUpdate.value.phone = userRef.value.phone
 }
 watch(() => userRef.value, initUserInfos)
 
 const initUserGroups = () => {
-    let groups: (string | undefined)[] = []
-    props.user?.groups.forEach((group: UserGroup) => {
-        groups.push(getGroupLiteral(group.groupId))
-    })
-    groups = groups.filter((element, index) => {
-        return groups.indexOf(element) === index
-    })
-    userGroups.value = groups.join(', ')
+  let groups: (string | undefined)[] = []
+  props.user?.groups.forEach((group: UserGroup) => {
+    groups.push(getGroupLiteral(group.groupId))
+  })
+  groups = groups.filter((element, index) => {
+    return groups.indexOf(element) === index
+  })
+  userGroups.value = groups.join(', ')
 }
 watch(() => userRef.value, () => {
-    initUserInfos()
-    initUserGroups()
+  initUserInfos()
+  initUserGroups()
 })
 
 onMounted(() => {
-    initUserInfos()
-    initUserGroups()
+  initUserInfos()
+  initUserGroups()
 })
 
 </script>
 
 <template>
-    <fieldset>
+  <fieldset>
+    <QInput
+        v-model="userToUpdate.firstName"
+        :disable="!!props.user?.isCas"
+        :label="t('forms.first-name')"
+        :rules="[val => val && val.length > 0 || t('forms.required-first-name')]"
+        filled
+        lazy-rules
+    />
+    <QInput
+        v-model="userToUpdate.lastName"
+        :disable="!!props.user?.isCas"
+        :label="t('forms.last-name')"
+        :rules="[val => val && val.length > 0 || t('forms.required-last-name')]"
+        filled
+        lazy-rules
+    />
+    <section class="email-modification">
+      <QInput
+          v-model="userToUpdate.email"
+          :label="t('forms.email')"
+          :rules="[(val, rules) => rules.email(val) || t('forms.required-email')]"
+          disable
+          filled
+          lazy-rules
+      />
+      <QExpansionItem
+          v-if="!props.user?.isCas"
+          v-model="changeEmail"
+          :label="t('forms.modify-email')"
+      >
         <QInput
-            v-model="userToUpdate.firstName"
-            :disable="!!props.user?.isCas"
-            :label="t('forms.first-name')"
-            :rules="[val => val && val.length > 0 || t('forms.required-first-name')]"
+            v-model="userToUpdate.newEmail"
+            :label="t('forms.new-email')"
+            :rules="changeEmail ? [(val, rules) => rules.email(val) || t('forms.required-new-email')] : []"
             filled
             lazy-rules
         />
         <QInput
-            v-model="userToUpdate.lastName"
-            :disable="!!props.user?.isCas"
-            :label="t('forms.last-name')"
-            :rules="[val => val && val.length > 0 || t('forms.required-last-name')]"
+            v-model="userToUpdate.newEmailVerification"
+            :label="t('forms.repeat-email')"
+            :rules="changeEmail ? [(val, rules) => (rules.email(val) && val === userToUpdate.newEmail) || t('forms.required-repeat-email')] : []"
             filled
             lazy-rules
         />
-        <section class="email-modification">
-            <QInput
-                v-model="userToUpdate.email"
-                :label="t('forms.email')"
-                :rules="[(val, rules) => rules.email(val) || t('forms.required-email')]"
-                disable
-                filled
-                lazy-rules
-            />
-            <QExpansionItem
-                v-if="!props.user?.isCas"
-                v-model="changeEmail"
-                :label="t('forms.modify-email')"
-            >
-                <QInput
-                    v-model="userToUpdate.newEmail"
-                    :label="t('forms.new-email')"
-                    :rules="changeEmail ? [(val, rules) => rules.email(val) || t('forms.required-new-email')] : []"
-                    filled
-                    lazy-rules
-                />
-                <QInput
-                    v-model="userToUpdate.newEmailVerification"
-                    :label="t('forms.repeat-email')"
-                    :rules="changeEmail ? [(val, rules) => (rules.email(val) && val === userToUpdate.newEmail) || t('forms.required-repeat-email')] : []"
-                    filled
-                    lazy-rules
-                />
-                <QBanner class="bg-grey-3">
-                    <template v-slot:avatar>
-                        <QIcon color="primary" name="mdi-alert-circle"/>
-                    </template>
-                    <span>{{ t('alerts.modify-email') }}</span>
-                </QBanner>
-            </QExpansionItem>
-        </section>
-        <QInput
-            v-model="userToUpdate.phone"
-            :label="t('forms.phone')"
-            filled
-            hint="Format : 06 00 00 00 00"
-            lazy-rules
-            mask="## ## ## ## ##"
-            type="tel"
-        />
-        <QBanner
-            v-if="!editedByStaff"
-            class="bg-grey-3 status-banner"
-        >
-            <template v-slot:avatar>
-                <QIcon color="primary" name="mdi-account"/>
-            </template>
-            <p>{{ t('dashboard.my-status') }} <span>{{ userGroups }}</span></p>
-            <p>{{ t('dashboard.update-my-status') }}</p>
+        <QBanner class="bg-grey-3">
+          <template v-slot:avatar>
+            <QIcon color="secondary" name="mdi-alert-circle"/>
+          </template>
+          <span>{{ t('alerts.modify-email') }}</span>
         </QBanner>
-    </fieldset>
+      </QExpansionItem>
+    </section>
+    <QInput
+        v-model="userToUpdate.phone"
+        :label="t('forms.phone')"
+        filled
+        hint="Format : 06 00 00 00 00"
+        lazy-rules
+        mask="## ## ## ## ##"
+        type="tel"
+    />
+    <QBanner
+        v-if="!editedByStaff"
+        class="bg-grey-3 status-banner"
+    >
+      <template v-slot:avatar>
+        <QIcon color="secondary" name="mdi-account"/>
+      </template>
+      <p>{{ t('dashboard.my-status') }} <span>{{ userGroups }}</span></p>
+      <p>{{ t('dashboard.update-my-status') }}</p>
+    </QBanner>
+  </fieldset>
 </template>
 
 <style lang="sass">
@@ -136,16 +136,16 @@ onMounted(() => {
 
 <style lang="sass" scoped>
 .q-expansion-item
-    background-color: #beddff
-    padding: 10px
-    margin-bottom: 15px
+  background-color: #beddff
+  padding: 10px
+  margin-bottom: 15px
 
 .status-banner
-    margin-top: 15px
-    margin-bottom: 15px
-    padding-top: 20px
+  margin-top: 15px
+  margin-bottom: 15px
+  padding-top: 20px
 
 fieldset
-    border: none
-    padding: 0
+  border: none
+  padding: 0
 </style>
--- src/views/PasswordResetView.vue
+++ src/views/PasswordResetView.vue
@@ -1,11 +1,11 @@
 <script lang="ts" setup>
 import FormPassWordReset from '@/components/form/FormPasswordReset.vue'
-import { useI18n } from "vue-i18n";
+import {useI18n} from "vue-i18n";
 
-const { t } = useI18n()
+const {t} = useI18n()
 </script>
 
 <template>
   <h1>{{ t("password.reset-password") }}</h1>
-  <FormPassWordReset />
+  <FormPassWordReset/>
 </template>
