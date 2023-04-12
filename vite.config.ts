bd7162768dc9c193cd43968763f712476aa05789
Carlo Spiga <cspiga@unistra.fr>
2023-04-12 09:08:53 +0200

    🚨 Forgot linting in previous commit.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { fileURLToPath, URL } from 'url'
+import {fileURLToPath, URL} from 'url'
 
-import { defineConfig } from 'vite'
-import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+import {defineConfig} from 'vite'
+import {quasar, transformAssetUrls} from '@quasar/vite-plugin'
 import vue from '@vitejs/plugin-vue'
 
 
@@ -9,7 +9,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
     plugins: [
         vue({
-            template: { transformAssetUrls }
+            template: {transformAssetUrls}
         }),
 
         quasar({
