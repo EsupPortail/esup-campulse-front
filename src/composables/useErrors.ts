9b431a6d06599b18141b86b7de8b9a00b5230691
pgilg <pgilg@unistra.fr>
2023-04-19 15:27:25 +0200

    ♻️ Update HTTP error catching function

--- src/composables/useErrors.ts
+++ src/composables/useErrors.ts
@@ -4,13 +4,11 @@ export default function () {
         const HTTP_ERROR_CODES = [400, 401, 403, 404, 405, 413, 415]
         let notification = ''
         if (HTTP_ERROR_CODES.includes(code)) {
-            notification = code.toString()
+            notification = code.toString() + '-'
         } else if (code >= 500) {
-            notification = '500'
-        } else {
-            notification = 'loading'
+            notification = '500-'
         }
-        notification += '-error'
+        notification += 'error'
         return notification
     }
 
