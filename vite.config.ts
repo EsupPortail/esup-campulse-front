197cbc16b92bffd97dd097f15f7476aedce305b1
Carlo Spiga <cspiga@unistra.fr>
2023-04-25 08:20:45 +0200

    🔧 Trying to change vue-i18n call after CSP change.

--- vite.config.ts
+++ vite.config.ts
@@ -23,6 +23,7 @@ export default defineConfig({
             '~': fileURLToPath(new URL('./tests', import.meta.url)),
             // Enable this line to disable script unsafe-eval in Nginx config.
             'vue': fileURLToPath(new URL('./node_modules/vue/dist/vue.runtime.esm-bundler.js', import.meta.url)),
+            'vue-i18n': fileURLToPath(new URL('./node_modules/vue-i18n/dist/vue-i18n.esm-browser.js', import.meta.url)),
         },
     },
     server: {
