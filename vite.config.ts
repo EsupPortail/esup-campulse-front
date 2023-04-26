73e78bff8c8de6187a613a1caa9f5b04db641db7
Carlo Spiga <cspiga@unistra.fr>
2023-04-26 15:41:54 +0200

    🔧 Trying to remove unsafe-eval CSP.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import {fileURLToPath, URL} from 'url'
 
 import {defineConfig} from 'vite'
+import {resolve, dirname} from 'node:path'
 import {quasar, transformAssetUrls} from '@quasar/vite-plugin'
 import vue from '@vitejs/plugin-vue'
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 
 
 // https://vitejs.dev/config/
@@ -14,6 +16,11 @@ export default defineConfig({
         quasar({
             autoImportComponentCase: 'pascal'
         }),
+        VueI18nPlugin({
+            /* options */
+            // locale messages resource pre-compile option
+            include: resolve(dirname(fileURLToPath(import.meta.url)), './src/locales/**'),
+        }),
     ],
     resolve: {
         alias: {
@@ -22,7 +29,7 @@ export default defineConfig({
             '~': fileURLToPath(new URL('./tests', import.meta.url)),
             // Enable these lines to disable script unsafe-eval in Nginx config.
             'vue': fileURLToPath(new URL('./node_modules/vue/dist/vue.runtime.esm-bundler.js', import.meta.url)),
-            'vue-i18n': fileURLToPath(new URL('./node_modules/vue-i18n/dist/vue-i18n.runtime.esm-bundler.js', import.meta.url)),
+            'vue-i18n': fileURLToPath(new URL('./node_modules/vue-i18n/dist/vue-i18n.runtime.esm-bundler.prod.js', import.meta.url)),
         },
     },
     server: {
