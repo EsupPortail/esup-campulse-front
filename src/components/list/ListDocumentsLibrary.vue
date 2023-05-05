84e45fdbca74b163a182c7eda85493b24ade2a9b
pgilg <pgilg@unistra.fr>
2023-05-05 15:17:07 +0200

    🏷️ Fix misimported type

--- src/components/list/ListDocumentsLibrary.vue
+++ src/components/list/ListDocumentsLibrary.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import {DocumentProcessType} from '#/documents'
+import type {DocumentProcessType} from '#/documents'
 import {useI18n} from 'vue-i18n'
 
 const {t} = useI18n()
