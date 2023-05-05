d192c5ce1df13d4b3e17344d9442b9de0effcbb8
pgilg <pgilg@unistra.fr>
2023-05-05 10:08:08 +0200

    🚚 Rename TemplateDocumentsLibraryView -> ManageDocumentsLibraryView

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -312,7 +312,7 @@ const routes: RouteRecordRaw[] = [
                     {
                         path: 'template-documents-library',
                         name: 'TemplateDocumentsLibrary',
-                        component: () => import('@/views/dashboard/TemplateDocumentsLibraryView.vue'),
+                        component: () => import('@/views/dashboard/ManageDocumentsLibraryView.vue'),
                         meta: {
                             breadcrumb: i18n.global.t('breadcrumbs.template-documents-library'),
                             title: i18n.global.t('breadcrumbs.template-documents-library'),
