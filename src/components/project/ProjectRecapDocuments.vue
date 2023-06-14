92e27e3585e698ef203a148b6f7d21b90b82ba58
pgilg <pgilg@unistra.fr>
2023-06-14 14:25:55 +0200

    🏷️ Fix type imports on views

--- src/components/project/ProjectRecapDocuments.vue
+++ src/components/project/ProjectRecapDocuments.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import useProjectDocuments from '@/composables/useProjectDocuments'
-import {ProcessDocument} from '#/documents'
+import type {ProcessDocument} from '#/documents'
 import axios from 'axios'
 import {useQuasar} from 'quasar'
 import {useI18n} from 'vue-i18n'
