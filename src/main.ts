fa574f789c8d52afca20976382b906665eb98979
Carlo Spiga <cspiga@unistra.fr>
2023-06-23 15:59:57 +0200

    ⬆️ Updated minor dependencies.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
-import {createApp} from 'vue'
-import {createPinia} from 'pinia'
-import {Loading, Notify, Quasar} from 'quasar'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { Loading, Notify, Quasar } from 'quasar'
 import quasarLang from 'quasar/lang/fr'
 import quasarIconSet from 'quasar/icon-set/svg-mdi-v6'
 import '@quasar/extras/mdi-v6/mdi-v6.css'
 import 'quasar/dist/quasar.css'
 import * as Sentry from '@sentry/vue'
-import {BrowserTracing} from '@sentry/tracing'
 import App from '@/App.vue'
 import router from '@/router'
 import i18n from '@/plugins/i18n'
@@ -24,7 +23,7 @@ if (import.meta.env.PROD) {
         app,
         dsn: import.meta.env.VITE_APP_SENTRY_DSN,
         integrations: [
-            new BrowserTracing({
+            new Sentry.BrowserTracing({
                 routingInstrumentation: Sentry.vueRouterInstrumentation(router),
                 tracingOrigins: ['localhost', import.meta.env.VITE_APP_FRONT_URL, /^\//]
             }),
