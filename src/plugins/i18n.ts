2fcff57f55ac70321b76f401bafcb3c1f4ddbfbf
Carlo Spiga <cspiga@unistra.fr>
2023-08-30 15:44:39 +0200

    ⏪️ Rollback changes on locale Plugin (deploy failed).

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -1,7 +1,9 @@
 import {createI18n} from 'vue-i18n'
 
+import fr from '@/locales/fr.json'
+
 const appLocale = import.meta.env.VITE_APP_I18N_LOCALE
-const {default: localeFile} = await import(`../locales/${import.meta.env.VITE_APP_I18N_LOCALE}.json`)
+
 
 // localStorage.getItem('octantLocale') || navigator.language.split('-')[0] || appLocale
 
@@ -14,6 +16,6 @@ export default createI18n({
     locale,
     fallbackLocale: appLocale,
     messages: {
-        fr: localeFile,
+        fr
     },
 })
