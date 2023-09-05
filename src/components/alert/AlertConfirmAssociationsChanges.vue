b51a6b50f58ab32f6323e0268c15e9352e7ec7ef
Carlo Spiga <cspiga@unistra.fr>
2023-09-05 10:02:04 +0200

    ✏️ #160 Removed associations names from AlertConfirmAssociationsChanges CSV export.

--- src/components/alert/AlertConfirmAssociationsChanges.vue
+++ src/components/alert/AlertConfirmAssociationsChanges.vue
@@ -115,12 +115,12 @@ async function onConfirmChanges(emailType: string) {
                 const message = t('notifications.positive.csv-export-associations')
                 notify({
                     type: 'positive',
-                    message: `<p>${message}${associationNames.join(', ')}</p>`,
+                    message,
                     html: true
                 })
                 const link = document.createElement('a')
                 link.href = window.URL.createObjectURL(new Blob([file]))
-                link.download = `${t('association.csv-name')}${encodeURI(associationNames.join('-'))}.csv`
+                link.download = `${t('association.csv-name')}.csv`
                 document.body.appendChild(link)
                 link.click()
                 link.remove()
