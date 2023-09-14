0125d8b9d79ab4b701d69432022b6f65201e28a8
pgilg <pgilg@unistra.fr>
2023-09-14 09:22:10 +0200

    ✅ Add tests for useErrors

--- src/composables/__tests__/useErrors.spec.ts
+++ src/composables/__tests__/useErrors.spec.ts
@@ -6,9 +6,39 @@ describe('useErrors', () => {
     const {catchHTTPError} = useErrors()
 
     describe('catchHTTPError', () => {
-        describe('if the error code is 4XX', () => {
+        describe('if the error code is 400', () => {
+            it('should return the corresponding notification according to HTTP error code', () => {
+                expect(catchHTTPError(400)).toEqual(i18n.global.t('notifications.negative.error-400'))
+            })
+        })
+        describe('if the error code is 401', () => {
+            it('should return the corresponding notification according to HTTP error code', () => {
+                expect(catchHTTPError(401)).toEqual(i18n.global.t('notifications.negative.error-401'))
+            })
+        })
+        describe('if the error code is 403', () => {
             it('should return the corresponding notification according to HTTP error code', () => {
                 expect(catchHTTPError(403)).toEqual(i18n.global.t('notifications.negative.error-403'))
+            })
+        })
+        describe('if the error code is 404', () => {
+            it('should return the corresponding notification according to HTTP error code', () => {
+                expect(catchHTTPError(404)).toEqual(i18n.global.t('notifications.negative.error-404'))
+            })
+        })
+        describe('if the error code is 405', () => {
+            it('should return the corresponding notification according to HTTP error code', () => {
+                expect(catchHTTPError(405)).toEqual(i18n.global.t('notifications.negative.error-405'))
+            })
+        })
+        describe('if the error code is 413', () => {
+            it('should return the corresponding notification according to HTTP error code', () => {
+                expect(catchHTTPError(413)).toEqual(i18n.global.t('notifications.negative.error-413'))
+            })
+        })
+        describe('if the error code is 415', () => {
+            it('should return the corresponding notification according to HTTP error code', () => {
+                expect(catchHTTPError(415)).toEqual(i18n.global.t('notifications.negative.error-415'))
             })
         })
         describe('if the error is 5XX', () => {
