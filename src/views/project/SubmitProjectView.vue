594f27c67efd84bcace8b96c8d99f4a063612668
Pauline Gilg <pgilg@unistra.fr>
2026-06-12 16:31:35 +0200

    🚸 Project name must be 99 chars or less

--- src/views/project/SubmitProjectView.vue
+++ src/views/project/SubmitProjectView.vue
@@ -486,7 +486,8 @@ onBeforeRouteLeave(reInitSubmitProjectForm)
                             <QInput
                                 v-model="projectBasicInfos.name"
                                 :label="t('project.name') + ' *'"
-                                :rules="[val => val && val.length > 0 || t('forms.required-project-name')]"
+                                :rules="[val => val && val.length > 0 || t('forms.required-project-name'),
+                                         val => val && val.length < 100 || t('forms.project-name-under-100')]"
                                 aria-required="true"
                                 clearable
                                 color="commission"
@@ -497,7 +498,8 @@ onBeforeRouteLeave(reInitSubmitProjectForm)
                             <QInput
                                 v-model="projectBasicInfos.plannedStartDate"
                                 :label="t('project.planned-start-date') + ' *'"
-                                :rules="[val => val && val.length > 0 || t('forms.required-project-startdate'), val => val && datesAreLegal || t('forms.legal-dates')]"
+                                :rules="[val => val && val.length > 0 || t('forms.required-project-startdate'),
+                                         val => val && datesAreLegal || t('forms.legal-dates')]"
                                 aria-required="true"
                                 clearable
                                 color="commission"
